23e0aa59fa8e92a9f6592b8ac2568687a08eebe9
Ross <rmojgani99@gmail.com>
2019-07-05 20:12:49 -0700

    Changed node.js to use express framework instead of http module

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 const port = 48714
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+app.get('/', (reqeust, response) => {
+  response.send('Hello from Express!')
+})
 
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if(err) {
-    return console.log('Something bad happened', err)
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
   }
-
-  console.log(`server is listening on ${port}`)
+ 
+  console.log(`server is listening on port ${port}`)
 })
