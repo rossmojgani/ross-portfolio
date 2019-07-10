a1c340fcb7ac7061c44bd02185a37870edfe1cc3
Ross <rmojgani99@gmail.com>
2019-07-09 19:42:08 -0700

    Added links to github, linkedin, and contact page with logos from fontawesome.com

--- client/src/components/Landingpage.js
+++ client/src/components/Landingpage.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import avatar from './avatar.jpg';
 
-const landingpage = () => (
-  <div>
-    landing page.
-  </div>
-)
+class landingpage extends Component {
+  render() {
+   return(
+    <div style={{width: '100%', margin: 'auto'}}>
+      <Grid className="landing-grid">
+        <Cell col={12}>
+         <img 
+           src={avatar}
+           alt={"avatar"}
+           className="avatar-img"
+         />
+         
+         <div className="banner-text">
+           <h1>Computer Engineering Student</h1>
+           <hr />
+           <p>Python | C++ | C | Java | Web Development </p>
+          
+         <div className="social-links">
+           {/* github link */}
+           <a href="https://github.com/rossmojgani" rel="noopener noreferrer" target="_blank">
+             <i className="fa fa-github-square" aria-hidden="true" />
+           </a>
+           
+           {/* linkedin link */}
+           <a href="https://www.linkedin.com/in/rossmojgani/" rel="noopener noreferrer" target="_blank">
+             <i className="fa fa-linkedin-square" aria-hidden="true" />
+           </a>
+           
+           {/* resume link */}
+           <a href="/contact" rel="noopener noreferrer" target="_blank">
+             <i className="fa fa-address-card" aria-hidden="true" />
+           </a>
+          </div>
+         
+         </div>
+        </Cell>
+      </Grid>
+    </div>
+    )
+  }
+}
 
 export default landingpage
