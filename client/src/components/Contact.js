d66b4078ae316f90adea4366297fe2696a13ceeb
Ross <rmojgani99@gmail.com>
2019-07-11 19:00:43 -0700

    Changed font sizes in contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -22,28 +22,28 @@ class Contact extends Component {
             <List>
               
               <ListItem>
-                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
                   <i className="fa fa-phone-square" aria-hidden="true" />
                   (123)  456-7890
                 </ListItemContent>
               </ListItem>
               
               <ListItem>
-                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
                   <i className="fa fa-fax" aria-hidden="true" />
                   (123)  456-7890
                 </ListItemContent>
               </ListItem>
               
               <ListItem>
-                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                <ListItemContent style={{fontSize: '15px', fontFamily: 'Anton'}}>
                   <i className="fa fa-envelope" aria-hidden="true" />
                   someone@example.com
                 </ListItemContent>
               </ListItem>
         
               <ListItem>
-                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
                   <i className="fa fa-skype" aria-hidden="true" />
                   My skype id
                 </ListItemContent>
