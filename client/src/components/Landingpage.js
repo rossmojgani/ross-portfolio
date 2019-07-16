a67de6b833d2e314a73669a1d872e265d3159651
Ross <rmojgani99@gmail.com>
2019-07-15 22:09:25 -0700

    Typist working on landingpage

--- client/src/components/Landingpage.js
+++ client/src/components/Landingpage.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import avatar from './avatar.jpg';
 import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 
 class landingpage extends Component {
+  state = {typing: true}
+
+  done = () => {
+    this.setState({ typing : false }, () => {
+      this.setState({ typing: true })
+    });
+  }
+    
   render() {
-   return(
+    const {children} = this.props;
+    return(
     <div style={{width: '100%', margin: 'auto'}}>
       <Grid className="landing-grid">
         <Cell col={12}>
@@ -17,7 +27,19 @@ class landingpage extends Component {
          
          <div className="banner-text">
            <h1>Ross Mojgani</h1>
-           <h1><Typist>Computer Engineering Student.</Typist></h1>
+           
+           <h1>
+           {this.state.typing ? <Typist onTypingDone={this.done}>
+                 Computer Engineering Student.
+             <Typist.Backspace count={29} delay={2500} />
+                 Software Engineer Intern.
+             <Typist.Backspace count={25} delay={2500} />
+                 Embedded Software Developer.
+             <Typist.Backspace count={28} delay={2500} />
+           </Typist> : <h1><Typist onTypingDone={this.done}>Test</Typist></h1> 
+           }
+           </h1>
+
            <hr />
            <p>Python | Java | C++ | C | Web Development </p>
          <div className="social-links">
