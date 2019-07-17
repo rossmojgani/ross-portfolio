40dc0472eaab86498555783e3da7bee0cfe4b7bd
Ross <rmojgani99@gmail.com>
2019-07-16 21:48:42 -0700

    Added inital cards to experience page

--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import '../css/Experience.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-const Experience = () => (
-  <div> Experience </div>
-)
+class Experience extends Component {
+  render() {
+    return(
+      <div className="experience-cards">
+      <Card>
+        <Card.Header>Ecoation</Card.Header>
+        <Card.Body>
+          <Card.Title>Embedded Software Engineer</Card.Title>
+          <Card.Text>
+            Hello world
+          </Card.Text>
+          <Button variant="primary">Github</Button>
+        </Card.Body>
+      </Card>
+      <hr/>
+      <Card>
+        <Card.Header>Ecoation</Card.Header>
+        <Card.Body>
+          <Card.Title>Embedded Software Engineer</Card.Title>
+          <Card.Text>
+            Hello world
+          </Card.Text>
+          <Button variant="primary">Github</Button>
+        </Card.Body>
+      </Card>
+      </div>
+    )
+  }
+
+
+}
 
 export default Experience
 
