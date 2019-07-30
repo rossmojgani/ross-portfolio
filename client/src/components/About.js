5fef2cc056b43611adcf3cd4fb7da6382e769e6b
Ross <rmojgani99@gmail.com>
2019-07-29 21:53:03 -0700

    added content and formatted about me page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,14 +8,16 @@ class About extends Component {
     return(
       <div className="about-body" id={'about'}>
         <Grid className="about-grid">
-          <Cell col={12}>
+          <Cell col={6}>
           <h2>Ross Mojgani</h2>
           <img 
             src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
             alt="avatar"
-            style={{height: '250px'}}
           />
-            <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique leo sem, in sollicitudin ipsum iaculis blandit. Fusce faucibus porta eros, a finibus elit iaculis in. Maecenas tempor fermentum orci ac rhoncus. Maecenas volutpat dolor ac consequat faucibus. Phasellus non mauris suscipit lorem hendrerit consequat vitae non nibh.</p>
+          </Cell>
+          <Cell col={6}>
+            <h2>About me</h2>
+            <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>I'm a 3rd year UBC Computer Engineering Student currently on an 8 month internship as an Embedded Software Engineer at Ecoation Innovative Solutions. <br/><br/>  In my spare time I am a Software Developer for the UBC Mars Colony Engineering Design Team, and am working with the software sub-team on developing code to control lighting and door systems and read various industry level sensors (CO2, Humidity, Pressure, Temperature, O2) with I2C communication protocols. <br/><br/> I'm interested in collaborating with others towards interesting projects, or to discuss future technologies. Contact me using any of the links above or below and I'll get back to you as soon as I can. </p>
           </Cell>
         </Grid>
         </div>
